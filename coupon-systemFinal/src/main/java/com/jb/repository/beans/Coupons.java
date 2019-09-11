b8d84531ff841c758e92e30d787ccdee1ed33737
Yocheved  Tochner <44393518+yochevedt@users.noreply.github.com>
2019-09-11 12:07:05 +0300

    added beans package

--- coupon-systemFinal/src/main/java/com/jb/repository/beans/Coupons.java
+++ coupon-systemFinal/src/main/java/com/jb/repository/beans/Coupons.java
@@ -1,5 +1,10 @@
 package com.jb.repository.beans;
 
+/************************************************
+***this classes in beans will be : Coupons-CouponType  Customer - Company
+*the annotations that will be used: @Entity, @Table @Column, @GeneratedId, @Id**/
+
+
 public class Coupons {
 
 }
